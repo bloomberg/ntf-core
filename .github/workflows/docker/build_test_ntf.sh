0e60afe075699c129077e30f53ef24aa73c821b0
Matt Millett <mmillett@mailbox.org>
2023-12-11 09:09:08 -0500

    GitHub Actions: stop on first failure and cat ctest log

--- .github/workflows/docker/build_test_ntf.sh
+++ .github/workflows/docker/build_test_ntf.sh
@@ -1,28 +1,60 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
-
-configure() {
-    cd /workspace
-    PATH="$PATH:$(realpath srcs/bde-tools/bin)"
-    export PATH
-    eval "$(bbs_build_env -u opt_64_cpp17)"
-}
+# configure() {
+#     cd /workspace
+#     PATH="$PATH:$(realpath srcs/bde-tools/bin)"
+#     export PATH
+#     eval "$(bbs_build_env -u opt_64_cpp17)"
+# }
 
 build_ntf() {    
+    local rc=0
+    
     cd /workspace/ntf-core
-    sed -i s/CMakeLists.txt//g ./configure
-    ./configure --prefix /opt/bb
-    make -j8
-    make test | tee test.log
+    rc=${?}
+    if [ ${rc} -ne 0 ]; then
+        echo "Failed to change directory: rc = ${rc}"
+        exit ${rc}
+    fi
 
-    if cat test.log | grep -q "Errors while running CTest"; then
-        echo "There are errors while running UTs, exiting..."
-        exit 1
+    # sed -i s/CMakeLists.txt//g ./configure
+    
+    ./configure --prefix /opt/bb
+    rc=${?}
+    if [ ${rc} -ne 0 ]; then
+        echo "Failed to configure: rc = ${rc}"
+        exit ${rc}
+    fi
+
+    make build
+    rc=${?}
+    if [ ${rc} -ne 0 ]; then
+        echo "Failed to build: rc = ${rc}"
+        exit ${rc}
+    fi
+
+    make build_test
+    rc=${?}
+    if [ ${rc} -ne 0 ]; then
+        echo "Failed to build tests: rc = ${rc}"
+        exit ${rc}
+    fi
+
+    make test
+    rc=${?}
+    if [ ${rc} -ne 0 ]; then
+        cat /workspace/ntf-core/build/Testing/Temporary/LastTest.log
+        echo "Failed to build tests: rc = ${rc}"
+        exit ${rc}
     fi
 
     make install
+    rc=${?}
+    if [ ${rc} -ne 0 ]; then
+        echo "Failed to install: rc = ${rc}"
+        exit ${rc}
+    fi
 }
 
-configure
+# configure
 build_ntf
