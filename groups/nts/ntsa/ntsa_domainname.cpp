45c6dfca3abb11944e1ee8b8a9b4aec2eaa4f4c3
Evgenii Malygin <2518859+678098@users.noreply.github.com>
2026-05-13 06:36:56 -0400

    Update ntsa_domainname.cpp

--- groups/nts/ntsa/ntsa_domainname.cpp
+++ groups/nts/ntsa/ntsa_domainname.cpp
@@ -245,7 +245,7 @@ bool DomainName::parse(const bslstl::StringRef& name,
             if (i == 0) {
                 return false;
             }
-            else if (domain[i - 1] == '.') {
+            else if (d_buffer[name_size + 1] == '.') {
                 return false;
             }
         }
